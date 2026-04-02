f76688ab4a0fe29196cb829501142ddb135912c0
Heyang Liu <lhypds@gmail.com>
2026-04-02 18:43:35 +0900

    Small fix

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,3 +1,3 @@
 export { function1 } from './function1';
 export { function2 } from './function2';
-export { parseFunctionQuery } from '../utils/functionUItls';
+export { parseFunctionQuery } from '../utils/functionUtils';
