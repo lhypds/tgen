f942e298cb6f259cb3ee177be35ec7b1855ca380
Heyang Liu <lhypds@gmail.com>
2026-04-03 02:37:09 +0900

    Remove serve

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,11 @@
-const fs = require("fs");
-const path = require("path");
-
-// Read PORT from .env
-let port = 3190;
-try {
-  const env = fs.readFileSync(path.join(__dirname, ".env"), "utf8");
-  const match = env.match(/^PORT=(\d+)/m);
-  if (match) port = Number(match[1]);
-} catch { }
-
 module.exports = {
   apps: [
     {
       name: "tgen",
-      script: "node_modules/.bin/serve",
-      args: ["-s", "dist", "-l", String(port)],
+      script: "node_modules/.bin/vite",
+      args: "preview",
       cwd: __dirname,
+      env_file: ".env",
       env: {
         NODE_ENV: "production",
       },
