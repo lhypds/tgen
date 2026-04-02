6d47bc175c03d698025654ca7ec3765bf8682b21
Heyang Liu <lhypds@gmail.com>
2026-04-03 02:13:21 +0900

    Small fix

--- start.sh
+++ start.sh
@@ -4,9 +4,6 @@
 PORT=$(grep -E '^PORT=' .env 2>/dev/null | cut -d '=' -f2)
 PORT=${PORT:-3190}
 
-echo "Building..."
-npm run build || { echo "Build failed"; exit 1; }
-
 if pm2 start ecosystem.config.cjs; then
   echo "Server started at http://localhost:$PORT"
 fi
